3acbd61bc326c4f0a57c9500f68b7e3d656aeea8
axure <96837058+Axure0@users.noreply.github.com>
2024-11-27 23:46:53 +0000

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,1 +1,23 @@
-a
+local function Hook_Adonis(meta_defs)
+    for _, tbl in pairs(meta_defs) do
+      for i, func in pairs(tbl) do
+        if type(func) == "function" and islclosure(func) then
+          hookfunction(func, function()
+            return pcall(coroutine.close, coroutine.running())
+          end)
+        end
+      end
+    end
+  end
+    
+local function Init_Bypass()
+  for i, v in getgc(true) do
+    if typeof(v) == "table" and rawget(v, "indexInstance") and rawget(v, "newindexInstance") and rawget(v, "namecallInstance") and type(rawget(v, "newindexInstance")) == "table" then
+      if v["newindexInstance"][1] == "kick" then
+        Hook_Adonis(v)
+      end
+    end
+  end
+end
+    
+Init_Bypass()
