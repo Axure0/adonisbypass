be000bb7f1340ab9b5e204b0346af945eb1c7706
axure <96837058+Axure0@users.noreply.github.com>
2024-11-30 19:45:40 +0000

    Update main.lua

--- main.lua
+++ main.lua
@@ -2,7 +2,8 @@
 
 for _, value in getgc(true) do 
     if type(value) == 'table' then
-       if rawget(value, "indexInstance") or rawget(value, "newindexInstance") or rawget(value, “newIndexInstance”) then 
+       if rawget(value, "indexInstance") or rawget(value, "newindexInstance") or rawget(value, “newIndexInstance”) or rawget(value,
+"namecallInstance") then 
           value.tvk = { "kick", function() return task.wait(9e9) end } 
        end
     end
