b2e0b67368e54272c183da85b722128e0fa52ba6
axure <96837058+Axure0@users.noreply.github.com>
2024-11-30 20:18:10 +0000

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,11 +1,17 @@
 -- CREDITS TO A GUY ON V3RM FOR MAKING THIS
 
-for k, v in pairs(getgc(true)) do
+for k, v in getgc(true) do
    if pcall(function()
       return rawget(v, "indexInstance")
    end) and type(rawget(v, "indexInstance")) == "table" and (rawget(v, "indexInstance"))[1] == "kick" then
-      v.tvk = {"kick", function()
-        return game.Workspace:WaitForChild("")
-      end}
+      for i,c in pairs(v) do
+          for ix,ca in pairs(c) do
+             if type(ca) == "function" then
+                ca = function()
+                   task.wait(9e9)
+                end
+             end
+          end
+       end
    end
 end
